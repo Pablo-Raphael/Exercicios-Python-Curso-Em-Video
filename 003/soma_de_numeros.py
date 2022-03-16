0832bc3f0de032ccb74cdd3a3ceeec49a2f236c7
Pablo Raphael <pabloraphael358@gmail.com>
2022-03-16 16:39:18 -0300

    Adição do 3 exercicio

---
+++ 003/soma_de_numeros.py
@@ -0,0 +1,7 @@
+# Enunciado:
+# Escreva um programa que receba do usuário 2 números e mostre a soma deles na tela
+
+# Solução:
+n1 = int(input('Digite o 1* número : '))
+n2 = int(input('Digite o 2* número : '))
+print('A soma entre', n1, 'e', n2, 'é', n1+n2)
