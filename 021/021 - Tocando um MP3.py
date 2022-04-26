02af2092b333d127c82d3130555f167f1e2a1f36
Pablo Raphael <pabloraphael358@gmail.com>
2022-04-25 21:30:54 -0300

    apagando pasta idea

---
+++ 021/021 - Tocando um MP3.py
@@ -0,0 +1,13 @@
+# Enunciado:
+
+# Faça um programa em Python que abra e reproduza o áudio de um arquivo MP3.
+
+# Solução:
+
+
+# instale essa biblioteca com o comando abaixo
+# pip install playsound
+from playsound import playsound
+
+# Use a funç abaixo ão da biblioteca para reproduzir o arquivo
+playsound('021.mp3')
