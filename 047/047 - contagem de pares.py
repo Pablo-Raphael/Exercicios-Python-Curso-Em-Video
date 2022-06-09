718c9c811927a8dd5fea5b8982d4b010b666493c
Pablo Raphael <pabloraphael358@gmail.com>
2022-06-09 20:11:52 -0300

    exercício 047 completo

---
+++ 047/047 - contagem de pares.py
@@ -0,0 +1,12 @@
+# Enunciado:
+
+"""
+Crie um programa que mostre na tela todos os números pares que estão no intervalo entre 1 e 50.
+"""
+
+# Solução:
+
+
+# Para cada número num intervalo de 1 a 50(é sempre 1 a menos, por isso 51) pulando de 2 em 2.
+for num in range(0, 51, 2):
+    print(num)
