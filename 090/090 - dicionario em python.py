04bd53b146215c19efa54feecc4c578a99a2cdb2
Pablo Raphael <pabloraphael358@gmail.com>
2022-07-30 18:12:45 -0300

    exercício 090 completo

---
+++ 090/090 - dicionario em python.py
@@ -0,0 +1,22 @@
+# Enunciado:
+
+"""
+Faça um programa que leia nome e média de um aluno, guardando também a situação em um dicionário.
+No final, mostre o conteúdo da estrutura na tela.
+"""
+
+# Solução:
+
+dicionario = {
+    'Nome': input('Digite o nome do aluno : '),
+    'Media': float(input(f'Digite a média do aluno : ')),
+}
+
+dicionario['Situação'] = 'Aprovado' if dicionario['Media'] >= 7 else 'Reprovado'
+
+print('-'*50)
+
+for chave, valor in dicionario.items():
+    print(f'- {chave} é {valor}')
+    
+print('-'*50)
